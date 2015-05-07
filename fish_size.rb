22d6ca2de49f334846146db92ce5fcbfce606588
Aaron Wiggins <AaronWiggins@Aarons-MacBook-Pro.local>
2015-05-06 23:31:30 -0400

    alternative answer, more methods

--- fish_size.rb
+++ fish_size.rb
@@ -1,11 +1,40 @@
+
 def size(min, max)
   rand(min..max)
 end
-puts "what kind of fish was it?"
-fish = gets.chomp
-puts "what's the smallest it could be?"
-min = gets.chomp.to_i
-puts "what's the biggest it could be?"
-max = gets.chomp.to_i
 
-puts "Man that " + "#{fish}" + " " "had to have been AT LEAST "+ "#{size(min, max)}" " feet long!"
+def prompt
+  puts "what kind of fish was it?"
+  fish = gets.chomp
+  puts "what's the smallest it could be?"
+  min = gets.chomp.to_i
+  puts "what's the biggest it could be?"
+  max = gets.chomp.to_i
+  [fish, min, max]
+  puts "Man that " + "#{fish}" + " " "had to have been AT LEAST "+
+  "#{size(min, max)}" " feet long!"
+end
+
+prompt
+
+# def get_fish
+#   puts "what kind of fish was it?"
+#   fish = gets.chomp
+# end
+#
+# def get_min
+#   puts "what's the smallest it could be?"
+#   min = gets.chomp.to_i
+# end
+#
+# def get_max
+#   puts "what's the biggest it could be?"
+#   max = gets.chomp.to_i
+# end
+
+# fish = get_fish
+# min = get_min
+# max = get_max
+
+# puts "Man that " + "#{fish}" + " " "had to have been AT LEAST "+
+# "#{size(min, max)}" " feet long!"
